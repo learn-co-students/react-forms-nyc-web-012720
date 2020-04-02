a63ae92b4e3240deb59f9e066f6d8cb21782e8c1
sekoudosso82 <sekou.dosso82@gmail.com>
2020-04-02 08:13:02 -0400

    Done.

---
+++ src/components/parentComponent.js
@@ -0,0 +1,50 @@
+// src/components/ParentComponent
+import React from 'react';
+import Form from './Form'
+import DisplayData from './displayData'
+
+ 
+class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+ 
+  // handleFirstNameChange = event => {
+  //   this.setState({
+  //     firstName: event.target.value
+  //   })
+  // }
+ 
+  // handleLastNameChange = event => {
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <Form
+            formData={this.state}
+            // handleFirstNameChange={this.handleFirstNameChange}
+            // handleLastNameChange={this.handleLastNameChange}
+            handleChange={this.handleChange}
+          />
+
+          <DisplayData formData={this.state} />
+
+      </div>
+      
+
+    )
+  }
+}
+ 
+export default ParentComponent;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/parentComponent'
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent/>
   </div>,
-  document.getElementById('root')
+  
+document.getElementById('root')
 );
